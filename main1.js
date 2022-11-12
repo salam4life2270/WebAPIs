e100b02a1e84b7b33c70ebd9369b5d71a7b328b9
rashidisalami <salam4life2270@yahoo.com>
2022-11-12 02:37:48 +0000

    finishing calculator

--- main1.js
+++ main1.js
@@ -1,24 +1,66 @@
 
-/** Defining contant variables **/
+/** Declaring the variables **/
+
+let currentInput = document.querySelector('.currentInput');
+let answerScreen = document.querySelector('.answerScreen');
+let buttons = document.querySelectorAll('button');
+let erasebtn = document.querySelector('#erase');
+let clearbtn = document.querySelector('#clear');
+let evaluate = document.querySelector('#evaluate');
 
 
-let numberButtons = document.querySelectorAll('[data-number]')
-let operationButtons = document.querySelectorAll('[data-operation]')
-let equalsButton = document.querySelector('data-equals')
-let allClearButton = document.querySelector('data-all-clear')
-let previousOperandTextElement = document.querySelector('data-current-input')
-let currentOperandTextElement = document.querySelector('data-answer-screen')
+// The calculator display
+let realTimeScreenValue = []
+
+//To clear
+clearbtn.addEventListener("click", () => {
+  realTimeScreenValue =[''];
+  answerScreen.innerHTML = 0;
+  currentInput.className = 'currentInput';
+  answerScreen.className = 'answerScreen';
+  answerScreen.style.color = 'greenyellow';
+});
+
+// Getting the value of button clicked
+buttons.forEach((btn) => {
 
 
+  btn.addEventListener("click", () => {
+      // when clicked button is not erased button 
+      if (!btn.id.match('erase')) {
+          // To display value on btn press
+          realTimeScreenValue.push(btn.value)
+          currentInput.innerHTML = realTimeScreenValue.join('');
 
+          // To evaluate answer in real time
+          if (btn.classList.contains('num_btn')) {
 
-class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-      this.previousOperandTextElement = previousOperandTextElement
-      this.currentOperandTextElement = currentOperandTextElement
-      this.clear()
-  }
-} 
+              answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+
+          }
+
+      }
+
+      // When erase button is clicked
+      if (btn.id.match('erase')) {
+          realTimeScreenValue.pop();
+          currentInput.innerHTML = realTimeScreenValue.join('');
+          answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+      }
+
+      // When clicked button is evaluate button
+      if (btn.id.match('evaluate')) {
+          currentInput.className = 'answerScreen';
+          answerScreen.className = 'currentInput';
+          answerScreen.style.color = "white";
+      }
+      // To prevent undefined error in screen
+      if (typeof eval(realTimeScreenValue.join('')) == 'undefined') {
+          answerScreen.innerHTML = 0
+      }
+
+   })
+});
 
 
 
